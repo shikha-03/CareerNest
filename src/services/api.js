01aac76c17722271ec14a692ba7023044c4126b3
shikha-03 <shikha99873@gmail.com>
2026-06-19 23:53:55 +0530

    Increase API timeout for deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api",
-  timeout: 10000
+  timeout: 60000
 });
 
 api.interceptors.request.use((config) => {
